4dd6ea268a4370108c593dcef75cd98243533139
killips <nicokillips@gmail.com>
2024-10-23 15:14:34 -0400

    about page - spacing and typography refinements of several elements, start of breaking out css into components

--- assets/js/ys-scripts.js
+++ assets/js/ys-scripts.js
@@ -52,47 +52,12 @@ if (document.querySelector('body.single-project')) {
     }
 }
 
-if (document.querySelector('body.page-about')) {
-    const skewed_image_containers = document.querySelectorAll('.ys-masonry-grid a');
-    const skewed_images = document.querySelectorAll('.ys-masonry-grid a img');
+const empty_paragraphs = document.querySelectorAll('p:empty')
+
+empty_paragraphs.forEach(elem => {
+    elem.remove();
+});
 
 
-    const founders_skewed_container_row = document.querySelector('.skewed-row');
-    const founders_skewed_image_container = document.querySelector('.skewed-img-container');
-    const founders_skewed_image = document.querySelector('.skewed-img-container img');
-
-
-
-
-    function set_image_widths() {
-        if (window.innerWidth > 1024) {
-            skewed_images.forEach(element => {
-                const parentWidth = element.parentElement.getBoundingClientRect().width.toString()
-                //console.log(element.getBoundingClientRect().width.toString())
-                element.style.width = parentWidth + 'px'
-            });
-
-            const founders_skewed_image_parent_width = founders_skewed_image.parentElement.getBoundingClientRect().width.toString()
-            const founders_skewed_image_parent_height = founders_skewed_container_row.getBoundingClientRect().height.toString()
-
-
-            founders_skewed_image_container.style.height = founders_skewed_image_parent_height + 'px'
-            founders_skewed_image.style.width = 'calc(50% + ' + founders_skewed_image_parent_width + 'px)'
-            founders_skewed_image.style.height = founders_skewed_image_parent_height + 'px'
-        } else {
-            skewed_images.forEach(element => {
-                element.style.width = '100%'
-            });
-
-            founders_skewed_image_container.style.height = 'auto'
-            founders_skewed_image.style.width = '100%'
-            founders_skewed_image.style.height = 'auto'
-        }
-    }
-
-    set_image_widths()
-
-    window.addEventListener("resize", set_image_widths)
-}
 
 
--- page-about.php
+++ page-about.php
@@ -19,10 +19,10 @@ $partners = get_field('partners');
     </div>
 </section>
 
-<section class="ys-section pad-top--xl">
+<section id="projects" class="ys-section pad-top--xxl block--masonry-grid">
     <div class="inner gutter">
         <h2 class="heading-l">Projects</h2>
-        <div class="ys-masonry-grid pad-top--l">
+        <div class="grid pad-top--l">
         <?php if ($projects):
             foreach ($projects as $project):
                 $pr_id = $project->ID;
@@ -31,28 +31,24 @@ $partners = get_field('partners');
                 $project_title = get_the_title($pr_id);
                 $project_link = get_permalink($pr_id);
                 ?>
-            <div class="ys-masonry-grid--col">
                 <a href="<?php echo esc_url($project_link); ?>">
                     <img class="" src="<?php echo $project_image['url']; ?>" alt="<?php echo $project_image['alt']; ?>">
                 </a>
-            </div>
             <?php endforeach; ?>
         <?php endif; ?>
         </div>
-        <a href="/projects" class="ys-btn ys-btn--yellow ys-btn--skewed"><span>All Projects</span></a>
+        <a href="/projects" class="ys-btn ys-btn--yellow"><span>All Projects</span></a>
     </div>
 </section>
 
-<section class="ys-section pad-top--xl">
+<section id="founders" class="ys-section pad-top--xxl block--two-col-img founders-section">
     <div class="inner gutter">
-
         <h2 class="heading-xl">Founders</h2>
         <div class="skewed-row">
             <div class="skewed-col skewed-col--first">
-                <div class="skew-shape--start"></div>
                 <h3><?php echo $founders_heading ?></h3>
                 <p><?php echo $founders_description ?></p>
-                <a class="ys-btn ys-btn--skewed text--black ys-btn--yellow" href="<?php echo $founders_link['url']; ?>" target="_blank"><?php echo $founders_link['title']; ?></a>
+                <a class="ys-btn ys-btn--yellow" href="<?php echo $founders_link['url']; ?>" target="_blank"><span><?php echo $founders_link['title']; ?></span></a>
             </div>
             <div class="skewed-col skewed-col--last skewed-img-container">
                 <img src="<?php echo $founders_image['url']; ?>" alt="<?php echo $founders_image['alt']; ?>">
@@ -61,9 +57,9 @@ $partners = get_field('partners');
     </div>
 </section>
 
-<section class="ys-section pad-top--xl">
+<section id="press" class="ys-section pad-top--xxl block--two-col-img">
     <div class="inner gutter">
-        <h2 class="heading-xl">Press</h2>
+        <div class="skewed-row skewed-row--reversed">
         <?php if ($featured_press):
             foreach ($featured_press as $press):
                 $pid = $press->ID;
@@ -74,20 +70,27 @@ $partners = get_field('partners');
                 $press_description = get_field('press_description', $pid);
                 $press_link = get_field('press_link', $pid);
                 ?>
-                <div class="ys-carousel-card--image-container">
-                    <?php echo get_the_post_thumbnail($pid, 'thumbnail'); ?>
-                    <a class="ys-btn ys-btn--yellow ys-skew" href="/press">All Press</a>
+                <div class="skewed-col skewed-col--first skewed-img-container">
+                    <h2 aria-hidden="true" class="heading-xl ys-skew--reversed show-mobile-only--block">Press</h2>
+                    <?php echo get_the_post_thumbnail($pid, ''); ?>
+                    <a class="ys-btn ys-btn--yellow show-desktop-only--flex" href="/press"><span>All Press</span></a>
                 </div>
-                <h3><?php echo $publication_name ?></h3>
-                <p class="ys-carousel-card--date"><?php echo $published_date ?></p>
-                <p class="ys-carousel-card--description"><?php echo $press_description ?></p>
-                <a class="ys-btn ys-btn--white ys-skew" href="<?php echo esc_url($press_link); ?>">Continue Reading</a>
+                <div class="skewed-col skewed-col--last">
+                    <h2 class="heading-xl ys-skew--reversed show-desktop-only--block">Press</h2>
+                    <h3><?php echo $publication_name ?></h3>
+                    <p class="ys-carousel-card--date"><?php echo $published_date ?></p>
+                    <p class=""><?php echo $press_description ?></p>
+                    <a class="ys-btn ys-btn--white" href="<?php echo esc_url($press_link); ?>"><span>Continue Reading</span></a>
+                    <a aria-hidden="true" class="ys-btn ys-btn--yellow show-mobile-only--flex" href="/press"><span>All Press</span></a>
+                </div>
+
             <?php endforeach; ?>
+        </div>
         <?php endif; ?>
     </div>
 </section>
 
-<section class="ys-section pad-top--xl">
+<section id="partners" class="ys-section pad-top--xl">
     <div class="inner gutter">
         <h2 class="heading-xl">Partners</h2>
         <?php
