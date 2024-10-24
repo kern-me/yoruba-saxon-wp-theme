dcdb846f18020119551887c13f820085033fafb7
killips <nicokillips@gmail.com>
2024-10-24 12:08:47 -0400

    about page - press section button group styling

--- page-about.php
+++ page-about.php
@@ -48,7 +48,7 @@ $partners = get_field('partners');
         <div class="skewed-row">
             <div class="skewed-col skewed-col--first">
                 <h3><?php echo $founders_heading ?></h3>
-                <p><?php echo $founders_description ?></p>
+                <p class="description"><?php echo $founders_description ?></p>
                 <a class="ys-btn ys-btn--yellow" href="<?php echo $founders_link['url']; ?>" target="_blank"><span><?php echo $founders_link['title']; ?></span></a>
             </div>
             <div class="skewed-col skewed-col--last skewed-img-container">
@@ -74,24 +74,36 @@ $partners = get_field('partners');
                 <div class="skewed-col skewed-col--first skewed-img-container">
                     <h2 aria-hidden="true" class="heading-xl ys-skew--reversed show-mobile-only--block">Press</h2>
                     <?php echo get_the_post_thumbnail($pid, ''); ?>
-                    <a class="ys-btn ys-btn--yellow show-desktop-only--flex" href="/press"><span>All Press</span></a>
                 </div>
                 <div class="skewed-col skewed-col--last">
                     <h2 class="heading-xl ys-skew--reversed show-desktop-only--block">Press</h2>
                     <h3><?php echo $publication_name ?></h3>
                     <p class="ys-carousel-card--date"><?php echo $published_date ?></p>
-                    <p class=""><?php echo $press_description ?></p>
-                    <a class="ys-btn ys-btn--white" href="<?php echo esc_url($press_link); ?>"><span>Continue Reading</span></a>
-                    <a aria-hidden="true" class="ys-btn ys-btn--yellow show-mobile-only--flex" href="/press"><span>All Press</span></a>
+                    <div class="description"><?php echo $press_description ?></div>
                 </div>
-
             <?php endforeach; ?>
         </div>
         <?php endif; ?>
+        <?php
+        if ($featured_press):
+            foreach ($featured_press as $press): ?>
+                <div class="btn-group">
+                    <div>
+                        <a class="ys-btn ys-btn--yellow" href="/press"><span>All Press</span></a>
+                    </div>
+                    <div>
+                        <?php $press_link = get_field('press_link', $press->ID); ?>
+                        <a class="ys-btn ys-btn--white" href="<?php echo esc_url($press_link); ?>"><span>Continue Reading</span></a>
+                    </div>
+                </div>
+            <?php
+            endforeach;
+        endif;
+        ?>
     </div>
 </section>
 
-<section id="partners" class="ys-section pad-top--xl">
+<section id="partners" class="ys-section pad-top--xxl">
     <div class="inner gutter">
         <h2 class="heading-xl">Partners</h2>
         <div class="flex-grid flex-grid--partners pad-top--l">
