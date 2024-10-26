ab9e49cbde0e15df87a276ac10e913fa18e70c60
killips <nicokillips@gmail.com>
2024-10-26 12:10:02 -0400

    global hero component - svg headings now individually implemented for each page to simplify the editing experience

--- template-parts/ys-hero.php
+++ template-parts/ys-hero.php
@@ -10,9 +10,16 @@ if ($media_selection === 'video') {
         '<section class="ys-hero ys-hero-video">
             <div class="video-container">
                 <video autoplay loop muted playsinline>
-                    <source src="' . $video_file . '#t='. $video_start_time . '" type="video/mp4">
+                    <source src="' . $video_file . '#t=' . $video_start_time . '" type="video/mp4">
                 </video>
             </div>';
+} else if ($media_selection === 'youtube') {
+    $youtubeID = get_field('youtubeID');
+    $youtube_title = get_field('youtube_title');
+    echo
+        '<section class="ys-hero ys-hero-video">
+            <iframe width="806" height="453" src="https://www.youtube.com/embed/' . $youtubeID . '" title="' . $youtube_title . '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+
 } else if ($media_selection === 'image') {
     $hero_image = get_field('hero_image');
 
@@ -23,16 +30,29 @@ if ($media_selection === 'video') {
     }
 }
 
-if (is_page() && !is_page('about')) {
-    $hero_heading_svg = get_field('hero_heading_svg');
-    echo '<img class="ys-hero-heading" src="' . esc_url($hero_heading_svg['url']) . '" alt="' . esc_attr(get_the_title()) . '" />';
-    echo '<h1 class="sr-only" aria-hidden="false">' . get_the_title() . '</h1>';
+if (is_page('home')) {
+    echo '<img class="ys-hero-heading" src="' . get_template_directory_uri() . '/assets/images/hero-heading-home.svg" alt="Yoruba Saxon" />';
+}
+
+if (is_page('press')) {
+    echo '<img class="ys-hero-heading" src="' . get_template_directory_uri() . '/assets/images/hero-heading-press.svg" alt="Press" />';
+}
+
+if (is_page('projects')) {
+    echo '<img class="ys-hero-heading" src="' . get_template_directory_uri() . '/assets/images/hero-heading-projects.svg" alt="Projects" />';
+}
+
+if (is_page('team')) {
+    echo '<img class="ys-hero-heading" src="' . get_template_directory_uri() . '/assets/images/hero-heading-team.svg" alt="Team" />';
 }
 
 if (is_page('about')) {
     echo '<h1><span>Stories</span> <span class="text--strikethrough">Untold</span></h1>';
 }
 
+if (is_page() && !is_page('about')) {
+    echo '<h1 class="sr-only" aria-hidden="false">' . get_the_title() . '</h1>';
+}
 
 if (is_single()) {
     echo '<h1 class="">' . get_the_title() . '</h1>';
