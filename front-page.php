4001154ded1eae0ed86f4cd4b3226f520ad4ed4a
killips <nicokillips@gmail.com>
2025-04-17 12:25:01 -0400

    #5 - Home page meet the team section. 3 column layout and image fields. ACF JSON files now in version control.

--- front-page.php
+++ front-page.php
@@ -76,6 +76,9 @@ $project_cards = get_field('project_cards');
 
 <?php
 $meet_the_team_image = get_field('meet_the_team_image');
+$meet_the_team_image_2 = get_field('meet_the_team_image_2');
+$meet_the_team_image_3 = get_field('meet_the_team_image_3');
+
 $meet_the_team_link = get_field('meet_the_team_link');
 
 if( $meet_the_team_link ):
@@ -84,15 +87,25 @@ if( $meet_the_team_link ):
     $link_target = $meet_the_team_link['target'] ? $meet_the_team_link['target'] : '_self';
 endif;
 ?>
-<section class="ys-section u-section--bg ys-section--meet-the-team ys-section--margin-top">
-    <div class="inner">
-        <a class="u-heading-link u-text-black u-show-mobile-only--inline-block" href="<?php echo site_url() ?>/projects">
-            <h2 class="u-heading-lt u-skew-base--desktop">Our Team</h2>
-        </a>
+<section class="ys-section ys-section--meet-the-team ys-section--margin-top">
+    <div class="meet-the-team--photo-grid_mobile">
+        <a class="u-heading-link u-text-black" href="<?php echo site_url() ?>/projects"><h2 class="u-heading-lt u-skew-base--desktop">Our Team</h2></a>
+        <img src="<?php echo esc_url($meet_the_team_image["url"]) ?>" alt="<?php echo $meet_the_team_image['alt'] ?>" />
+        <a class="ys-btn ys-btn--yellow u-margin-top--l" href="<?php echo esc_url( $link_url ) ?>" target="<?php echo esc_attr( $link_target ) ?>'"><span><?php echo esc_html( $link_title ) ?></span></a>
+    </div>
 
-        <a class="ys-btn ys-btn--white u-show-desktop-only--flex" href="<?php echo esc_url( $link_url ) ?>" target="<?php echo esc_attr( $link_target ) ?>'"><span><?php echo esc_html( $link_title ) ?></span></a>
-        <img src="<?php echo esc_url($meet_the_team_image["url"]) ?>" alt="" />
-        <a class="ys-btn ys-btn--yellow u-show-mobile-only--flex u-margin-top--l" href="<?php echo esc_url( $link_url ) ?>" target="<?php echo esc_attr( $link_target ) ?>'"><span><?php echo esc_html( $link_title ) ?></span></a>
+    <div class="meet-the-team--photo-grid">
+        <a class="ys-btn ys-btn--white" href="<?php echo esc_url( $link_url ) ?>" target="<?php echo esc_attr( $link_target ) ?>'"><span><?php echo esc_html( $link_title ) ?></span></a>
+
+        <img src="<?php echo esc_url($meet_the_team_image["url"]) ?>" alt="<?php echo $meet_the_team_image['alt'] ?>" />
+
+        <?php if( $meet_the_team_image_2): ?>
+            <img src="<?php echo esc_url($meet_the_team_image_2["url"]) ?>" alt="<?php echo $meet_the_team_image_2['alt'] ?>" />
+        <?php endif; ?>
+
+        <?php if( $meet_the_team_image_3): ?>
+            <img src="<?php echo esc_url($meet_the_team_image_3["url"]) ?>" alt="<?php echo $meet_the_team_image_3['alt'] ?>" />
+        <?php endif; ?>
     </div>
 </section>
 
